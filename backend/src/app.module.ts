ff7d0a21d44f8a85a2d37acbf8c388b029df8b5b
Phong Ly <lyquocphong@gmail.com>
2023-09-20 10:02:32 +0300

    Use config module for configuration handling
    - Add class-transform class-validator lib
    - Add config module from nest
    - Use config service to get port in app module and main.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from '@/utils/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration global
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  const appPort = configService.get<number>('PORT');
+
+  await app.listen(appPort);
 }
 bootstrap();

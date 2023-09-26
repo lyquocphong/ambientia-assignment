2e0e481ec3c064360c9859ca12bac70a2167b9a4
Phong Ly <lyquocphong@gmail.com>
2023-09-26 08:14:10 +0300

    Add missing app info route into backend

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
+import { getDefaultService } from '@/utils/service';
+import { getDefaultTimezone } from '@/utils/date';
+import { AppConfig } from '@/app.type';
 
 @Controller()
+@ApiTags('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('config')
+  getAppInfo(): AppConfig {
+    const service = getDefaultService();
+    const timezone = getDefaultTimezone();
+
+    return {
+      service,
+      timezone,
+    };
   }
 }

fe2d68c63eee7acf0cd1e1eadd8e97652b39568b
Phong Ly <lyquocphong@gmail.com>
2023-09-26 08:33:33 +0300

    remove unneeded files

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    return NextResponse.json({
-        service: {
-            name: 'Main hieronta'
-        }
-    })
-}
